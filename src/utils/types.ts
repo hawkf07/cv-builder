b14775031bbaf12e343f0468ecaa2f128a4c7f80
hawkf07 <hawkf07@proton.me>
2023-06-07 21:13:00 +0700

    fixing data types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,12 +3,15 @@ const skills = z.array(z.string());
 type a = z.infer<typeof skills>;
 
 export const contactInformationZodType = z.object({
-  address: z.string(),
-  email: z.string().email(),
-  linkedin: z.string(),
-  phoneNumber: z.string(),
+  contact: z.object({
+    address: z.string(),
+    email: z.string().email(),
+    linkedin: z.string(),
+    phoneNumber: z.string(),
+  }),
   summary: z.string(),
   skills: z.record(z.string(), z.string()),
+  experience: z.record(z.string(), z.string()),
 });
 
 export type data = z.infer<typeof contactInformationZodType>;
